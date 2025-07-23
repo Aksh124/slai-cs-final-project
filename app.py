9a29d89a2d919de52e179ed744c1b3513d1564c2
Akshay Mahajan <akshayxmahajan@gmail.com>
2025-07-22 21:45:07 -0500

    Co-authored-by: zhengd2 <zhengd2@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import app, Flask, render_template
+from os import abort
+from flask import app, Flask, render_template, current_app
 from logic import read_csv
 
 app = Flask(__name__)
@@ -7,13 +8,24 @@ data = read_csv("data/data_for_final.csv")
 
 @app.route("/")
 def index():
-    return render_template("base.html",data=data)
+    return render_template("index.html",data=data)
     
 @app.route("/table")
-def index_new():
+def show_table():
     return render_template("table_view.html",data=data)
-    
+'''
+@app.post("")
+def submit_form():
+    return render_template("", data=data)
+'''
+
+@app.route("/<string:major_name>")
+def college_by_name(major_name: str):
+    data2 = [ record for record in data if record["Level and field of highest degree"] == major_name]
+    if data2:
+        return data2
+    abort(404)
 
 #@app.route("/college_majors")
 #def return_majors():
-    
+#read_csv("data/data_for_final.csv"
